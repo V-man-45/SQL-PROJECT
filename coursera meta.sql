e64b99ad3b1ee8431b9d471264909d405bc19729
Sai Manoj Reddy Vancha <110759379+V-man-45@users.noreply.github.com>
2024-10-08 18:46:40 -0700

    Uploaded a file - building a database

---
+++ coursera meta.sql
@@ -0,0 +1,180 @@
+CREATE DATABASE IF NOT EXISTS Little_Lemoni;
+USE Little_Lemoni;
+CREATE TABLE Customers(CustomerID INT NOT NULL PRIMARY KEY, FullName VARCHAR(100) NOT NULL, PhoneNumber INT NOT NULL UNIQUE);
+INSERT INTO Customers(CustomerID, FullName, PhoneNumber) VALUES 
+(1, "Vanessa McCarthy", 0757536378), 
+(2, "Marcos Romero", 0757536379), 
+(3, "Hiroki Yamane", 0757536376), 
+(4, "Anna Iversen", 0757536375), 
+(5, "Diana Pinto", 0757536374),     
+(6, "Altay Ayhan", 0757636378),      
+(7, "Jane Murphy", 0753536379),      
+(8, "Laurina Delgado", 0754536376),      
+(9, "Mike Edwards", 0757236375),     
+(10, "Karl Pederson", 0757936374);
+CREATE TABLE Bookings (BookingID INT, BookingDate DATE,TableNumber INT, NumberOfGuests INT,CustomerID INT); 
+INSERT INTO Bookings 
+(BookingID, BookingDate, TableNumber, NumberOfGuests, CustomerID) 
+VALUES 
+(10, '2021-11-10', 7, 5, 1),  
+(11, '2021-11-10', 5, 2, 2),  
+(12, '2021-11-10', 3, 2, 4), 
+(13, '2021-11-11', 2, 5, 5),  
+(14, '2021-11-11', 5, 2, 6),  
+(15, '2021-11-11', 3, 2, 7), 
+(16, '2021-11-11', 3, 5, 1),  
+(17, '2021-11-12', 5, 2, 2),  
+(18, '2021-11-12', 3, 2, 4), 
+(19, '2021-11-13', 7, 5, 6),  
+(20, '2021-11-14', 5, 2, 3),  
+(21, '2021-11-14', 3, 2, 4);
+CREATE TABLE Courses (CourseName VARCHAR(255) PRIMARY KEY, Cost Decimal(4,2));
+INSERT INTO Courses (CourseName, Cost) VALUES 
+("Greek salad", 15.50), 
+("Bean soup", 12.25), 
+("Pizza", 15.00), 
+("Carbonara", 12.50), 
+("Kabasa", 17.00), 
+("Shwarma", 11.30);
+CREATE TABLE DeliveryAddress (ID INT PRIMARY KEY, Address VARCHAR(255) NOT NULL, Type VARCHAR(100) NOT NULL DEFAULT "Private", CustomerID INT NOT NULL, FOREIGN KEY(CustomerID) REFERENCES Customers(CustomerID) ON DELETE CASCADE ON UPDATE CASCADE); 
+ALTER TABLE DeliveryAddress ADD COLUMN Ingredients VARCHAR(255);
+
+
+
+SELECT * FROM Bookings WHERE BookingDate BETWEEN '2021-11-11' AND ' 2021-11-13';
+SELECT FullName, BookingID FROM Customers join Bookings on Customers.CustomerID = Bookings.CustomerID WHERE BookingDate = '2021-11-11';
+SELECT BookingDate, COUNT(BookingDate) FROM Bookings GROUP BY BookingDate;
+REPLACE INTO Courses (CourseName, Cost) VALUES ("Kabasa", 20.00);
+SELECT FullName  FROM Customers  WHERE (SELECT CustomerID FROM Bookings WHERE Customers.CustomerID = Bookings.CustomerID and BookingDate = "2021-11-11");
+CREATE VIEW BookingsView AS SELECT BookingID, BookingDate, NumberOfGuests FROM Bookings WHERE BookingDate < '2021-11-13' AND NumberOfGuests > 3;
+SELECT * FROM BookingsView;
+CREATE PROCEDURE GetBookingsData (InputDate DATE) SELECT * FROM Bookings WHERE BookingDate = InputDate;
+CALL GetBookingsData("2021-11-13");
+SELECT CONCAT("ID: ", BookingID,', Date: ', BookingDate,', Number of guests: ', NumberOfGuests) AS "Booking Details" FROM Bookings;
+
+
+
+
+-- create procedure and function
+
+CREATE DATABASE Lucky_Shrub;
+USE Lucky_Shrub;
+CREATE TABLE Orders (OrderID INT NOT NULL PRIMARY KEY, ClientID VARCHAR(10), ProductID VARCHAR(10), Quantity INT, Cost DECIMAL(6,2), Date DATE);
+INSERT INTO Orders(OrderID, ClientID, ProductID , Quantity, Cost, Date) VALUES
+(1, "Cl1", "P1", 10, 500, "2020-09-01"),  
+(2, "Cl2", "P2", 5, 100, "2020-09-05"),  
+(3, "Cl3", "P3", 20, 800, "2020-09-03"),  
+(4, "Cl4", "P4", 15, 150, "2020-09-07"),  
+(5, "Cl3", "P3", 10, 450, "2020-09-08"),  
+(6, "Cl2", "P2", 5, 800, "2020-09-09"),  
+(7, "Cl1", "P4", 22, 1200, "2020-09-10"),  
+(8, "Cl3", "P1", 15, 150, "2020-09-10"),  
+(9, "Cl1", "P1", 10, 500, "2020-09-12"),  
+(10, "Cl2", "P2", 5, 100, "2020-09-13"),  
+(11, "Cl4", "P5", 5, 100, "2020-09-15"), 
+(12, "Cl1", "P1", 10, 500, "2022-09-01"),  
+(13, "Cl2", "P2", 5, 100, "2022-09-05"),  
+(14, "Cl3", "P3", 20, 800, "2022-09-03"),  
+(15, "Cl4", "P4", 15, 150, "2022-09-07"),  
+(16, "Cl3", "P3", 10, 450, "2022-09-08"),  
+(17, "Cl2", "P2", 5, 800, "2022-09-09"),  
+(18, "Cl1", "P4", 22, 1200, "2022-09-10"),  
+(19, "Cl3", "P1", 15, 150, "2022-09-10"),  
+(20, "Cl1", "P1", 10, 500, "2022-09-12"),  
+(21, "Cl2", "P2", 5, 100, "2022-09-13"),   
+(22, "Cl2", "P1", 10, 500, "2021-09-01"),  
+(23, "Cl2", "P2", 5, 100, "2021-09-05"),  
+(24, "Cl3", "P3", 20, 800, "2021-09-03"),  
+(25, "Cl4", "P4", 15, 150, "2021-09-07"),  
+(26, "Cl1", "P3", 10, 450, "2021-09-08"),  
+(27, "Cl2", "P1", 20, 1000, "2022-09-01"),  
+(28, "Cl2", "P2", 10, 200, "2022-09-05"),  
+(29, "Cl3", "P3", 20, 800, "2021-09-03"),  
+(30, "Cl1", "P1", 10, 500, "2022-09-01");
+
+
+CREATE FUNCTION FindCosts( Order_ID INT)
+RETURNS DECIMAL(5,2) deterministic
+RETURN (SELECT Cost FROM Orders WHERE OrderID = Order_ID);
+
+SELECT FindCosts(5)
+
+
+
+DELIMITER // 
+CREATE PROCEDURE GetDIscountForProduct(OrderIDInput INT)
+
+     BEGIN 
+         DECLARE cost_after_discount DECIMAL(7,2); 
+         DECLARE current_cost DECIMAL(7,2); 
+         DECLARE order_quantity INT; 
+         SELECT Quantity INTO order_quantity FROM Orders WHERE OrderID = OrderIDInput; 
+         SELECT Cost INTO current_cost FROM Orders WHERE OrderID = OrderIDInput; 
+        IF order_quantity >= 20 THEN
+          SET cost_after_discount = current_cost - (current_cost * 0.2);              
+        ELSEIF order_quantity >= 10 THEN
+          SET cost_after_discount = current_cost - (current_cost * 0.1); 
+        ELSE SET cost_after_discount = current_cost;
+        END IF;
+    SELECT GetDIscountForProduct; 
+END//
+
+DELIMITER ; 
+
+
+-- trigger and events
+
+CREATE TABLE Products (ProductID VARCHAR(10), ProductName VARCHAR(100),BuyPrice DECIMAL(6,2), SellPrice DECIMAL(6,2), NumberOfItems INT);
+INSERT INTO Products (ProductID, ProductName, BuyPrice, SellPrice, NumberOfITems)
+
+VALUES ("P1", "Artificial grass bags ", 40, 50, 100),  
+("P2", "Wood panels", 15, 20, 250),  
+("P3", "Patio slates",35, 40, 60),  
+("P4", "Sycamore trees ", 7, 10, 50),  
+("P5", "Trees and Shrubs", 35, 50, 75),  
+("P6", "Water fountain", 65, 80, 15);
+
+CREATE TABLE Notifications (NotificationID INT AUTO_INCREMENT, Notification VARCHAR(255), DateTime TIMESTAMP NOT NULL, PRIMARY KEY(NotificationID));
+
+DELIMITER // 
+CREATE TRIGGER ProductSellPriceInsertCheck
+AFTER INSERT
+ON Products FOR EACH ROW
+BEGIN
+IF NEW.SellPrice <= NEW.BuyPrice THEN
+INSERT INTO Notifications (Notification,DateTime)
+VALUES (concat('A SellPrice same or less than the BuyPrice was inserted for ProductID ', NEW.ProductID), NOW());
+END IF;
+END//
+DELIMITER ; 
+
+
+DELIMITER //
+
+CREATE TRIGGER ProductSellPriceUpdateCheck 
+    AFTER UPDATE  
+    ON Products FOR EACH ROW  
+	BEGIN
+	IF NEW.SellPrice <= NEW.BuyPrice THEN
+		INSERT INTO Notifications(Notification,DateTime) 
+		VALUES(CONCAT(NEW.ProductID,' was updated with a SellPrice of ', NEW.SellPrice,' which is the same or less than the BuyPrice'), NOW()); 
+    END IF;
+	END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE TRIGGER NotifyProductDelete 
+    AFTER DELETE   
+    ON Products FOR EACH ROW   
+	INSERT INTO Notifications(Notification, DateTime) 
+    VALUES(CONCAT('The product with a ProductID ', OLD.ProductID,' was deleted'), NOW()); 
+END //
+DELIMITER ;
+
+
+
+
+
